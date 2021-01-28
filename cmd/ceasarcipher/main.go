8dab47011aef1fa43243f23215e6f7dfc8c9fca9
mova <valeriy.molchanov.77@gmail.com>
2021-01-28 21:28:14 +0200

    cipher

--- cmd/ceasarcipher/main.go
+++ cmd/ceasarcipher/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"bufio"
 	"flag"
-	"fmt"
+	"io"
 	"log"
 	"os"
+	"unicode"
 )
 
 func main() {
@@ -13,51 +14,49 @@ func main() {
 	file := flag.String("file", "", "input file (if empty - stdin)")
 	flag.Parse()
 
-	r := os.Stdin
+	f := os.Stdin
 	if *file != "" {
 		f, err := os.Open(*file)
 		if err != nil {
 			log.Fatalf("could not open file: %v\n", err)
 		}
 		defer f.Close()
-		r = f
 	}
 
 	n := *shiftFlag % 26
+	if n < 0 {
+		n += 26
+	}
 
-	s := bufio.NewScanner(r)
-	for s.Scan() {
-		in := s.Bytes()
-		out := make([]byte, len(in))
-		for i := 0; i < len(in); i++ {
-			out[i] = cipher(in[i], n)
+	reader := bufio.NewReader(f)
+
+	for {
+		r, _, err := reader.ReadRune()
+		if err == io.EOF {
+			return
 		}
-		fmt.Println(string(out))
-	}
-
-	if err := s.Err(); err != nil {
-		log.Fatalf("could not read: %v\n", err)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if r == unicode.ReplacementChar {
+			log.Println("invalid code point is found")
+			continue
+		}
+		os.Stdout.WriteString(string(cipher(r, n)))
 	}
 }
 
-func cipher(b byte, n int) byte {
-	if new, ok := shift(b, byte('a'), byte('z'), n); ok {
-		return new
+func cipher(r rune, n int) rune {
+	switch {
+	case r >= 'a' && r <= 'z':
+		return rotate(r, 'a', n)
+	case r >= 'A' && r <= 'B':
+		return rotate(r, 'A', n)
+	default:
+		return r
 	}
-	new, _ := shift(b, byte('A'), byte('Z'), n)
-	return new
 }
 
-func shift(b, min, max byte, n int) (byte, bool) {
-	if b < min || b > max {
-		return b, false
-	}
-	b = byte(int(b) + n)
-	if b > max {
-		b -= 26
-	}
-	if b < min {
-		b += 26
-	}
-	return b, true
+func rotate(r, base rune, n int) rune {
+	return base + (r-base+rune(n))%26
 }
